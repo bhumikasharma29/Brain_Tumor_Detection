70ba9163bb5a02745ec73ec74882f890a9277308
Bhumika Sharma <bhumikas850@gmail.com>
2021-06-17 15:24:15 +0530

    removing genvent

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from keras.initializers import glorot_uniform
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+#from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
